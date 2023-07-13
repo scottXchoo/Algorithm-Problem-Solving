fe8d5188a9d14451a75d1a497680560311a548ed
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-07-13 12:23:38 +0900

    [Bronze I] Title: 단어 공부, Time: 192 ms, Memory: 152236 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,13 @@
+words = input().upper()
+unique_words = list(set(words))
+
+cnt_list = []
+for x in unique_words:
+	cnt = words.count(x)
+	cnt_list.append(cnt)
+
+if cnt_list.count(max(cnt_list)) > 1:
+	print("?")
+else:
+	max_index = cnt_list.index(max(cnt_list))
+	print(unique_words[max_index])
