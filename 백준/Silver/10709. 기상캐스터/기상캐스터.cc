ddad18f9801a401e9e03d74d17c8aa7d3191a97a
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-08-28 17:47:25 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, a[104][104];
+string s;
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        cin >> s;
+        for(int j = 0; j < m; j++) {
+            if(s[j] == '.') a[i][j] = -1;
+            else a[i][j] = 0;
+        }
+    }
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(a[i][j] == 0) {
+                int cnt = 1;
+                while(a[i][j + 1] == -1) {
+                    a[i][j + 1] = cnt++;
+                    j++;
+                } 
+            }
+        }
+    }
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) cout << a[i][j] << " ";
+        cout << '\n';
+    }
+    return 0;
+}
