4f6c90a0aa08e1735a1ef500f115ac97d3f1b196
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-09-19 12:09:26 +0900

    Update 숨바꼭질 4.py

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -10,7 +10,10 @@ route = [0] * 100001
 def path(x):
   arr = []
   temp = x
+	# 시작점까지 포함해야하기 때문에 "+ 1"
   for _ in range(visited[x] + 1):
+		# 현재의 값을 arr에 넣고 그 위치에 할당된 값을 temp로 정의
+		## 이렇게 되면, 계속 역으로 시작점까지 가게 된다.
     arr.append(temp)
     temp = route[temp]
   return arr[::-1]
@@ -26,6 +29,7 @@ def bfs(x):
       if 0 <= nx < 100001 and visited[nx] == 0:
         dq.append(nx)
         visited[nx] = visited[x] + 1
+				# 전의 값을 현재 위치에 할당할 수 있음
         route[nx] = x
 
 print(bfs(n))
