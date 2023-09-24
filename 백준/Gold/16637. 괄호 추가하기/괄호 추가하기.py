85cba9388c6dfa29a687e61f80aef31ffba11755
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-09-24 12:42:36 +0900

    Update 괄호 추가하기.py

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
@@ -1,7 +1,6 @@
 import sys
-
-n = int(input())
-s = input()
+N = int(input())
+S = input()
 res = -1 * sys.maxsize
 
 def calc(num1, oper, num2):
@@ -15,16 +14,17 @@ def calc(num1, oper, num2):
 def dfs(idx, value):
   global res
 
-	# 계산 끝! 계속해서 최댓값 업데이트
-  if idx == n - 1:
+  # 계산 끝! 계속해서 최댓값 업데이트
+  if idx == N - 1:
     res = max(res, value)
     return
-	# 괄호 X
-  if idx + 2 < n:
-    dfs(idx + 2, calc(value, s[idx + 1], int(s[idx + 2])))
+  # 괄호 X
+  if idx + 2 < N:
+    dfs(idx + 2, calc(value, S[idx + 1], int(S[idx + 2])))
   # 괄호 O
-  if idx + 4 < n:
-    dfs(idx + 4, calc(value, s[idx + 1], calc(int(s[idx + 2]), s[idx + 3], int(s[idx + 4]))))
+  if idx + 4 < N:
+    next_calc = calc(int(S[idx + 2]), S[idx + 3], int(S[idx + 4]))
+    dfs(idx + 4, calc(value, S[idx + 1], next_calc))
 
-dfs(0, int(s[0]))
+dfs(0, int(S[0]))
 print(res)
