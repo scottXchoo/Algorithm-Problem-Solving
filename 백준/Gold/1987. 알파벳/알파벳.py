b3e06df74f4a127739a666f3fb3d1bed0d58e1a7
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-09-24 12:29:11 +0900

    Update 알파벳.py

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,5 +1,6 @@
-r, c = map(int, input().split())
-maps = [list(input()) for _ in range(r)]
+R, C = map(int, input().split())
+maps = [list(input()) for _ in range(R)]
+
 ans = 0
 alphas = set()
 dx = [-1, 1, 0, 0]
@@ -11,9 +12,15 @@ def dfs(x, y, cnt):
   
   for i in range(4):
     nx, ny = x + dx[i], y + dy[i]
-    if 0 <= nx < r and 0 <= ny < c and maps[nx][ny] not in alphas:
+    if not(0 <= nx < R and 0 <= ny < C):
+      continue
+    # 만약 alphas 안에 해당 알파벳이 없으면
+    if maps[nx][ny] not in alphas:
+      ## alphas 안에 해당 알파벳을 넣고
       alphas.add(maps[nx][ny])
+      ## 깊숙하게 dfs로 들어가서 cnt를 계속 올려준다
       dfs(nx, ny, cnt + 1)
+      ## 백트래킹 : 그러다 dfs가 끝나서 다시 처음으로 돌아오기 위해 해당 알파벳을 제거함으로써 초기화시킨다
       alphas.remove(maps[nx][ny])
 
 alphas.add(maps[0][0])
