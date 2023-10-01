1eaa635a0b9c10a62c05568688b3db0f78b14182
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-10-01 09:36:12 +0900

    [Bronze III] Title: 직각삼각형, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+while True :
+    nums = list(map(int, input().split()))
+    if sum(nums) == 0:
+        break  # 세 수가 0이면 break
+    max_num = max(nums)
+    nums.remove(max_num)  # 빗변의 길이는 직각삼각형 세변의 길이중 가장 길다.
+    if nums[0]**2 + nums[1]**2 == max_num**2:
+        print('right')
+    else:
+        print('wrong')
