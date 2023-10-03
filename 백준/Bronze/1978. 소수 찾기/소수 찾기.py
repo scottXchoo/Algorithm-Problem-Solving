96c1cbdcba7e5222d77a24aa1680f12c21b682a5
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-10-03 10:54:10 +0900

    Update 소수 찾기.py

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,12 +1,13 @@
-n = int(input())
-numbers = map(int, input().split())
+N = int(input())
+nums = map(int, input().split())
 sosu = 0
-for num in numbers:
-    error = 0
+
+for num in nums:
+    err = 0
     if num > 1 :
-        for i in range(2, num):  # 2부터 n-1까지
+        for i in range(2, num):  # 2부터 num - 1까지
             if num % i == 0:
-                error += 1  # 2부터 n-1까지 나눈 몫이 0이면 error가 증가
-        if error == 0:
-            sosu += 1  # error가 없으면 소수.
+                err += 1  # 2부터 num - 1까지 나눈 몫이 0이면 error가 증가
+        if err == 0:
+            sosu += 1  # error가 없으면 소수
 print(sosu)
