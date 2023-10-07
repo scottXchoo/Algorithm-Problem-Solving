4861e08d8176ca0b17d9f6efa577e1ce3d82c7a7
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-10-07 14:23:43 +0900

    [Silver IV] Title: 스택, Time: 160 ms, Memory: 116620 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+stack = []
+
+for _ in range(N):
+  s = sys.stdin.readline().split()
+  if s[0] == 'push':
+    stack.append(s[1])
+    
+  elif s[0] == 'pop':
+    if len(stack) > 0:
+      print(stack.pop())
+    else:
+      print(-1)
+      
+  elif s[0] == 'size':
+    print(len(stack))
+    
+  elif s[0] == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+      
+  elif s[0] == 'top':
+    if len(stack) > 0:
+      print(stack[-1])
+    else:
+      print(-1)
