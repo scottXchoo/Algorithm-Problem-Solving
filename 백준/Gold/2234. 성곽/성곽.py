bf18021302c5160f0b6116658a9119865e95ee84
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-10-17 12:15:19 +0900

    [Gold III] Title: 성곽, Time: 528 ms, Memory: 117136 KB -BaekjoonHub

---
+++ 백준/Gold/2234. 성곽/성곽.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+N, M = map(int, input().split())
+maps = [list(map(int, input().split())) for _ in range(M)]
+room_cnt, room_size = 0, 0
+visited = [[False] * N for _ in range(M)]
+dx, dy = [0, -1, 0, 1], [-1, 0, 1, 0]
+
+def isRange(x, y):
+  if 0 <= x < M and 0 <= y < N:
+    return True
+  return False
+
+def bfs(x, y):
+  dq = deque()
+  dq.append((x, y))
+  visited[x][y] = True
+  room = 1
+
+  while dq:
+    x, y = dq.popleft()
+    for i in range(4):
+      if maps[x][y] & (1 << i) == 0:
+        nx, ny = x + dx[i], y + dy[i]
+        
+        if isRange(nx, ny) is False:
+          break
+          
+        if visited[nx][ny] is False:
+          dq.append((nx, ny))
+          visited[nx][ny] = True
+          room += 1
+  return room
+
+for i in range(M):
+  for j in range(N):
+    if not visited[i][j]:
+      room_cnt += 1
+      room_size = max(room_size, bfs(i, j))
+
+print(room_cnt)
+print(room_size)
+
+room_del = 0
+for i in range(M):
+  for j in range(N):
+    for k in range(4):
+      if maps[i][j] & (1 << k) == (1 << k):
+        visited = [[False] * N for _ in range(M)]
+        maps[i][j] -= (1 << k)
+        room_del = max(room_del, bfs(i, j))
+        maps[i][j] += (1 << k)
+
+print(room_del)
