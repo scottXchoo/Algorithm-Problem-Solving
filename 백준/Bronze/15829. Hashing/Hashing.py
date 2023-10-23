6b00a83ad79d7e3a41fbc8fbdbb7dd836fcfe113
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-10-23 09:10:28 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 108080 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,12 @@
+L = int(input())
+strings = input()
+arr = []
+add, r = 0, 31
+
+for i in range(L):
+  arr.append(ord(strings[i]) - 96)
+
+for idx, num in enumerate(arr):
+  add += num * (r ** idx)
+
+print(add)
