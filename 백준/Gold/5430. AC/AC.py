1f4c143322fba1cda4a3e2979d457e107220400e
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-10-30 09:30:39 +0900

    [Gold V] Title: AC, Time: 244 ms, Memory: 159692 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,33 @@
+from collections import deque
+T = int(input())
+
+for _ in range(T):
+  f = input()
+  n = int(input())
+  arr = input()[1:-1].split(',')
+
+  dq = deque(arr)
+  flag = 0
+
+  if n == 0:
+    dq = deque([])
+
+  for i in f:
+    if i == 'R':
+      flag += 1
+    elif i == 'D':
+      if len(dq) == 0:
+        print("error")
+        break
+      else:
+        if flag % 2 == 0:
+          dq.popleft()
+        else:
+          dq.pop()
+
+  else:
+    if flag % 2 == 0:
+      print("[" + ",".join(dq) + "]")
+    else:
+      dq.reverse()
+      print("[" + ",".join(dq) + "]")
