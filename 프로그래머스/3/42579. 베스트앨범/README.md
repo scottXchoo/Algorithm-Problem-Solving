5fac5743c2c932d41ff3e37412dc8ec7d870f3f1
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-11-26 13:43:55 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.04 ms
+메모리: 10.4 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 9:48:50
+2023년 11월 0일 13:43:50
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,31 +1,14 @@
 def solution(genres, plays):
     answer = []
-    play_map = {}
-    genre_map = {}
-    # [1] genres를 play의 합을 기준으로 오름차순 정렬
-    for idx, genre in enumerate(genres):
-        if genre in genre_map:
-            genre_map[genre] += plays[idx]
-        else:
-            genre_map[genre] = plays[idx]
-    sorted_genre_map = sorted(genre_map.items(), key= lambda item:item[1], reverse=True)
-    # [2] 오름차순 정렬된 해시맵에서 genre를 기준으로 for문
-    for genre, _ in list(sorted_genre_map):
-        temp = []
-        # [3] 각 genre에 맞는 genres의 index를 구해 이를 plays의 값과 함께 temp에 대입
-        for idx, _genre in enumerate(genres):
-            if _genre == genre:
-                temp.append((plays[idx], idx))
-        # [4] temp의 0번째 요소는 오름차순, 1번째 요소는 내림차순 순서로 정렬
-        temp.sort(key=lambda x: (-x[0], x[1]))
-        # [5] 가장 많이 재생된 노래 최대 두 개씩만 보여주기 때문
-        temp = temp[:2]
-        # [6] 만약 (800, 0), (800, 3)과 같이 play가 중복된다면?
-        ## 아래와 같이 play+idx로 key를 만드는 방법으로 해결 (더 좋은 방법 없을까)
-        for play, idx in temp:
-            play_map[play+idx] = idx
+    genreDict = {genre:[] for genre in set(genres)} # {'classic': [], 'pop': []}
+    for e in zip(genres, plays, range(len(plays))): # ('classic', 500, 0), ('pop', 600, 1), ...
+        # genreDict[e[0]] : []
+        genreDict[e[0]].append([e[1], e[2]]) # {'pop': [[600, 1], [2500, 4]], 'classic': [[500, 0], [150, 2], [800, 3]]}
+    genreSort = sorted(list(genreDict.keys()), key = lambda x: sum(t[0] for t in genreDict[x]), reverse = True)
+    for genre in genreSort:
+        # e : [150, 2], e[1] : 고유 번호(2), x[0] : 재생된 횟수(150), x[1] : 고유 번호(2)
+        # x[0]은 내림차순 그 이후, x[1]은 오름차순
+        temp = [e[1] for e in sorted(genreDict[genre], key = lambda x: (x[0], -x[1]), reverse = True)]
+        answer += temp[:min(len(temp), 2)] # 최대 2까지 자르는 방법
 
-    for i in play_map:
-        answer.append(play_map[i])
-    
     return answer
