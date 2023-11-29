0641c277a7e54a3dcd01f1e84e8f98aaae46f219
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2023-11-29 11:33:09 +0900

    [Gold II] Title: 피자판매, Time: 232 ms, Memory: 121684 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.py
+++ 백준/Gold/2632. 피자판매/피자판매.py
@@ -1,34 +1,38 @@
-Target = int(input())
-M, N = map(int, input().split())
-a, b = [], []
-for _ in range(M):
-  a.append(int(input()))
-for _ in range(N):
-  b.append(int(input()))
+from collections import deque
+import sys
+ipt = sys.stdin.readline
 
-a_comb, b_comb = [0] * (sum(a)+1), [0] * (sum(b)+1)
-a_comb[0], b_comb[0] = 1, 1
-a_comb[-1], b_comb[-1] = 1, 1
 
-for j in range(1, M):
-  for i in range(M):
-    if i+j > M:
-      sum_a = sum(a[i:] + a[:(i+j)%M])
-    else:
-      sum_a = sum(a[i:i+j])
-    a_comb[sum_a] += 1
+def init():
+    total = int(ipt())
+    m, n = map(int, ipt().split())
+    a = [int(ipt()) for _ in range(m)]
+    b = [int(ipt()) for _ in range(n)]
+    return a, m, b, n, total
+    
 
-for j in range(1, N):
-  for i in range(N):
-    if i+j > N:
-      sum_b = sum(b[i:] + b[:(i+j)%N])
-    else:
-      sum_b = sum(b[i:i+j])
-    b_comb[sum_b] += 1
+def get_count(a, m):
+    count_a = [0] * (sum(a) + 1)
+    count_a[0] = 1
+    count_a[-1] = 1
+    for i in range(m):
+        q = deque(a)
+        for _ in range(i):
+            q.rotate(-1)
+        q.pop()
+        summ = 0
+        while q:
+            summ += q.popleft()
+            count_a[summ] += 1
+    return count_a
 
-ans = 0
-for i in range(Target+1):
-  j = Target - i
-  if 0 <= i < len(a_comb) and 0 <= j < len(b_comb):
-    ans += a_comb[i] * b_comb[j]
-print(ans)
+
+a, m, b, n, total = init()
+count_a = get_count(a, m)
+count_b = get_count(b, n)
+answer = 0
+for i in range(total + 1):
+    j = total - i
+    if 0 <= i < len(count_a) and 0 <= j < len(count_b):
+        answer += count_a[i] * count_b[j]
+print(answer)
