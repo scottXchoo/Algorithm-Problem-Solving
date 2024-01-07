20302f1ed786b7d2c32749ecd3e55e5ac3409431
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2024-01-07 16:05:45 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.55 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+                
+    for k in course:
+        candidates = []
+        for order in orders:
+            candidates += combinations(sorted(order), k)
+        common = Counter(candidates).most_common()
+        for c, cnt in common:
+            if cnt >= 2 and cnt == common[0][1]:
+                answer.append(''.join(c))
+
+    answer.sort()
+    return answer
