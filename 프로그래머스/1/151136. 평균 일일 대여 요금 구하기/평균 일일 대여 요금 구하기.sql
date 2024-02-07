51bda425892a9d6e3570f85b425acd1219f57070
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2024-02-07 14:43:48 +0900

    [level 1] Title: 평균 일일 대여 요금 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/151136. 평균 일일 대여 요금 구하기/평균 일일 대여 요금 구하기.sql
@@ -0,0 +1,11 @@
+# 자동차 종류가 'SUV'인 자동차들의 평균 일일대여 요금을 출력
+# 평균 일일 대여 요금은 소수 첫 번째 자리에서 반올림
+# 컬럼명은 AVERAGE_FEE
+# CAR_TYPE과 DAILY_FEE까지는 잘 꺼냈다.
+# 그 다음 궁금한 점 :
+## 1) 새로운 col(AVERAGE_FEE)는 어떻게 만들지?
+## 2) AVG()를 활용하는 것 맞나? 소수 첫 번째 자리에서 반올림 어떻게 하지?
+-- 코드를 입력하세요
+SELECT ROUND(AVG(DAILY_FEE), 0) as AVERAGE_FEE
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE CAR_TYPE = 'SUV'
