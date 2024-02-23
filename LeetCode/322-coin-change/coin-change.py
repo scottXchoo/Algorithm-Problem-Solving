4add6807412ed4a64615105c64770b6af92bbd31
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2024-02-24 01:41:57 +0900

    Time: 729 ms (84.77%) | Memory: 16.9 MB (77.64%) - LeetSync

--- LeetCode/322-coin-change/coin-change.py
+++ LeetCode/322-coin-change/coin-change.py
@@ -1,11 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [sys.maxsize for _ in range(amount + 1)]
-        dp[0] = 0
+        dp = [0] + ([float('inf')]) * amount
 
-        for i in range(amount + 1):
+        for i in range(1, amount + 1):
             for coin in coins:
-                if i + coin <= amount:
-                    dp[i+coin] = min(dp[i+coin], dp[i]+1)
-
-        return -1 if dp[amount] == sys.maxsize else dp[amount]
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        if dp[-1] == float('inf'):
+            return -1
+            
+        return dp[-1]
