83ba2188c673da0cd6bd000823ea4c458df8e11f
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2024-02-24 12:33:35 +0900

    [Gold III] Title: 연구소 3, Time: 296 ms, Memory: 117324 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -1,67 +1,59 @@
 from itertools import combinations
 from collections import deque
-import copy, sys
+import sys
 
 N, M = map(int, input().split())
 maps = [list(map(int, input().split())) for _ in range(N)]
 virus = []
-answer = sys.maxsize
+board = [["_"] * N for _ in range(N)]
+for i in range(N):
+  for j in range(N):
+    if maps[i][j] == 2:
+      virus.append((i, j))
+      board[i][j] = "*"
+    elif maps[i][j] == 1:
+      board[i][j] = "-"
 
-for r in range(N):
-  for c in range(N):
-    if maps[r][c] == 2:
-      virus.append([r, c])
-      maps[r][c] = "*"
-    elif maps[r][c] == 1:
-      maps[r][c] = "-"
-    elif maps[r][c] == 0:
-      maps[r][c] = "_"
+def is_in_range(r, c):
+  return 0 <= r < N and 0 <= c < N
 
-def is_range(next_r, next_c):
-  return 0 <= next_r < N and 0 <= next_c < N
-
-def bfs(v):
-  global answer
-  dr, dc = [0, 1, 0, -1], [1, 0, -1, 0]
-  q = deque()
-  for r, c in v:
-    q.append([r, c, 0])
-  temp = copy.deepcopy(maps) # [board[i][:] for i in range(N)]
+def bfs(virus, map):
   max_time = 0
-  
-  while q:
-    cur_r, cur_c, cur_t = q.popleft()
-    for i in range(4):
-      next_r = cur_r + dr[i]
-      next_c = cur_c + dc[i]
-      next_t = cur_t + 1
-      if is_range(next_r, next_c):
-        if temp[next_r][next_c] == "_":
-          temp[next_r][next_c] = next_t
-          max_time = max(max_time, next_t)
-          q.append([next_r, next_c, next_t])
-        elif temp[next_r][next_c] == "*":
-          temp[next_r][next_c] = next_t
-          q.append([next_r, next_c, next_t])
+  dq = deque()
+  for r, c in virus:
+    dq.append((r, c, 0))
+    map[r][c] = 0
 
-  # 바이러스가 다 퍼졌는데도 "_"가 있으면, answer 그대로
-  for r in range(N):
-    for c in range(N):
-      if temp[r][c] == "_":
-        return
-  # 바이러스가 잘 퍼졌으면, answer 업데이트
-  answer = min(answer, max_time)
-  return
+  while dq:
+    c_r, c_c, time = dq.popleft()
+    for dr, dc in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+      n_r = c_r + dr
+      n_c = c_c + dc
+      n_time = time + 1
+      if is_in_range(n_r, n_c):
+        if map[n_r][n_c] == "_": # 빈 칸이면?
+          dq.append((n_r, n_c, n_time))
+          map[n_r][n_c] = n_time
+          max_time = max(max_time, n_time)
+        elif map[n_r][n_c] == "*": # 비활성이면?
+          dq.append((n_r, n_c, n_time))
+          map[n_r][n_c] = n_time
 
-# 활성화될 바이러스 선택
-for v in combinations(virus, M):
-  for r, c in v:
-    maps[r][c] = 0
-  bfs(v)
-  for r, c in v:
-    maps[r][c] = "*"
+  for i in range(N):
+    for j in range(N):
+      if map[i][j] == "_":
+        return -1
+  else:
+    return max_time    
+
+answer = sys.maxsize
+for active_virus in combinations(virus, M):
+  temp = [item[:] for item in board]
+  time = bfs(active_virus, temp)
+  if time != -1:
+    answer = min(answer, time)
 
 if answer == sys.maxsize:
-  answer = -1
-
-print(answer)
+  print(-1)
+else:
+  print(answer)
