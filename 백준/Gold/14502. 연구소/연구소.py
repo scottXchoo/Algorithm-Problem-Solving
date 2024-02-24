64d17215eb92cc8344aa27fff9c2295501a753c6
scottXchoo <107841492+scottXchoo@users.noreply.github.com>
2024-02-24 12:27:20 +0900

    [Gold IV] Title: 연구소, Time: 496 ms, Memory: 115948 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,21 +1,3 @@
-'''
-<문제 분석>
-꼭 벽 3개를 다 세워야 된다.
-안전 영역 : 바이러스(2)가 다 퍼진 뒤, 0의 개수
-안전 영역 크기의 최댓값을 구하는 프로그램을 작성하시오
-
-<제한 조건>
-3 <= N, M <= 8
-2 <= 바이러스(2) <= 10
-
-<아이디어>
-1. 주어진 map에 벽 3개를 세운다.
- - 3개는 조합으로 정한다.
-2. 3개를 세운 뒤, 바이러스를 퍼뜨린다.
- - 바이러스의 위치도 필요하겠다.
-3. 바이러스가 다 퍼진 벽에 있는 0의 개수를 세고 업데이트한다.
-'''
-
 from itertools import combinations
 from collections import deque
 import copy
@@ -28,41 +10,44 @@ for i in range(N):
   for j in range(M):
     if maps[i][j] == 0:
       blanks.append((i, j))
-    if maps[i][j] == 2:
+    elif maps[i][j] == 2:
       virus.append((i, j))
 
 def is_in_range(r, c):
   return 0 <= r < N and 0 <= c < M
 
-def bfs(board):
+def bfs(map):
+  cnt = 0
   visited = [[0] * M for _ in range(N)]
-  q = deque()
-  for r, c in virus:
-    q.append((r, c))
-    visited[r][c] = 1
-  
-  while q:
-    c_r, c_c = q.popleft()
+  dq = deque()
+  for i, j in virus:
+    dq.append((i, j))
+    visited[i][j] = 1
+
+  while dq:
+    c_r, c_c = dq.popleft()
     for dr, dc in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
       n_r = c_r + dr
       n_c = c_c + dc
       if is_in_range(n_r, n_c) and not visited[n_r][n_c]:
-        if board[n_r][n_c] == 0:
-          q.append((n_r, n_c))
-          board[n_r][n_c] = 2
+        if map[n_r][n_c] == 0:
+          dq.append((n_r, n_c))
+          map[n_r][n_c] = 2
           visited[n_r][n_c] = 1
 
-answer = 0
-for combi in combinations(blanks, 3):
-  cnt = 0
-  temp = copy.deepcopy(maps)
-  for i, j in combi:
-    temp[i][j] = 1
-  bfs(temp)
   for i in range(N):
     for j in range(M):
-      if temp[i][j] == 0:
+      if map[i][j] == 0:
         cnt += 1
+
+  return cnt
+  
+answer = 0
+for combi in combinations(blanks, 3):
+  temp = [map[:] for map in maps]
+  for i, j in combi:
+    temp[i][j] = 1
+  cnt = bfs(temp)
   answer = max(answer, cnt)
 
 print(answer)
