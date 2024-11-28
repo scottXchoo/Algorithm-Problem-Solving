fc093928ab09a89826dab7e47639ba54f0b09157
Scott Choo (추교현) <107841492+scottXchoo@users.noreply.github.com>
2024-11-28 13:13:22 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 3984 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[500002];
+int N;
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+
+  int n, n_2, below_n;
+  int imsi_N, cnt, min_cnt;
+
+  cin >> N;
+
+  dp[0] = 0;  //1,4,9,16,25, ... 제곱수를 위함
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 3;
+  dp[4] = 1;
+
+  for (int i = 5; i <= N; i++) {
+	n = sqrt(i);
+	n_2 = n * n;
+	below_n = i - n_2;
+	min_cnt = 4;
+
+	for (int j = 1; j <= n; j++) {
+	  imsi_N = i - j * j;
+	  cnt = dp[imsi_N] + 1;
+	  if (cnt < min_cnt) {
+		min_cnt = cnt;
+	  }
+	}
+	dp[i] = min_cnt;
+  }
+
+  cout << dp[N];
+
+}
