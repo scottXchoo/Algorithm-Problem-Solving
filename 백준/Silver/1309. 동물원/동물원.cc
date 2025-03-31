31d2344ce5bcfcd779f88763138f9c712a082882
Scott Choo (추교현) <107841492+scottXchoo@users.noreply.github.com>
2025-03-31 19:08:01 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -1,25 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int dp[100001][3];
+int DP[100001] = {1,3};
 int mod = 9901;
 
 int main() {
-    int N;
-    cin >> N;
-    dp[0][0] = 1;
-    dp[0][1] = 1;
-    dp[0][2] = 1;
+  int N;
+  cin >> N;
 
-    for (int i = 1; i < N; i++) {
-        dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % mod;
-        dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % mod;
-        dp[i][2] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % mod;
-    }
+  for (int i = 2; i <= N; i++) {
+    DP[i] = (DP[i-2] + DP[i-1]*2) % mod;
+  }
 
-    const int answer = (dp[N-1][0] + dp[N-1][1] + dp[N-1][2]) % mod;
-    cout << answer << '\n';
-
-    return 0;
+  cout << DP[N];
 }
