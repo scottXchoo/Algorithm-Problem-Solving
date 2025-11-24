59128696c522e4ab11b53f1802c2e889b24cef11
Scott Choo (추교현) <107841492+scottXchoo@users.noreply.github.com>
2025-11-24 15:58:10 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 560 ms, Memory: 67412 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+#include <tuple>
+#define MAX 1005
+using namespace std;
+
+int N, M, K;
+int maps[MAX][MAX];
+queue<tuple<int, int, int, int>> q; // x, y, cnt, wall
+int visited[MAX][MAX][15];
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1, -1, 0, 0};
+
+void input() {
+    cin >> N >> M >> K;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            char input = '0';
+            cin >> input;
+            maps[i][j] = input - '0';
+        }
+    }
+}
+
+int bfs() {
+    q.push({1, 1, 1, 0});
+    visited[1][1][0] = 1;
+
+    while (!q.empty()) {
+        int cx = get<0>(q.front());
+        int cy = get<1>(q.front());
+        int cnt = get<2>(q.front());
+        int wall = get<3>(q.front());
+        q.pop();
+
+        if (cx == N && cy == M) {
+            return cnt;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+
+            if (nx <= 0 || nx > N || ny <= 0 || ny > M) continue;
+
+            int next_wall = wall + 1;
+            // 다음 칸 0이고 방문하지 않았다면
+            if (maps[nx][ny] == 0 && !visited[nx][ny][wall]) {
+                q.push({nx, ny, cnt + 1, wall});
+                visited[nx][ny][wall] = 1;
+            } else if (maps[nx][ny] == 1 && !visited[nx][ny][next_wall]) { // 다음 칸 1인데, 벽 부술 수 있다면
+                if (next_wall == K + 1) continue; // K까지는 가능
+                
+                q.push({nx, ny, cnt + 1, next_wall});
+                visited[nx][ny][next_wall] = 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main() {
+    input();
+
+    memset(visited, 0, sizeof(visited));
+    int answer = bfs();
+    cout << answer;
+
+    return 0;
+}
